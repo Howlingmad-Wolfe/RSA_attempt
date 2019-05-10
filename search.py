2e470517178f23805d26928dde37f795810301a2
Howlingmad-Wolfe <amadwolfe@gmail.com>
2019-05-10 17:18:14 -0500

    Added search.py
    Created a binary search tool for checking numbers produced by prime_tools.py prime() function against a large sample pool of known primes.

--- search.py
+++ search.py
@@ -10,37 +10,6 @@ class SearchTree(list):
             self.array = array
 
 
-    def search_2(self, subject, node=None, searchDepth=None):
-        if searchDepth == None:
-            searchDepth = 1
-            node = int(floor( len(self.array) / pow(2, searchDepth) ) )
-            print node, " root node\n"
-        else:
-            searchDepth += 1
-
-        block = float( len(self.array) / pow( 2, searchDepth ) )
-        print block, " float block\n"
-
-        if len(self.array) / pow( 2, searchDepth ) < 1.0:
-            return False
-
-        elif subject > self.array[node]:
-            node = node + int(floor(block))
-            print node, " next node\n"
-            return self.search(subject, node, searchDepth)
-
-        elif subject < self.array[node]:
-            node = node - int(floor( block ))
-            print node, " next node\n"
-            return self.search(subject, node, searchDepth)
-
-        elif subject == self.array[node]:
-            return True
-
-        else:
-            return False
-
-
     def search(self, subject):
         rangeStart = 0
         rangeStop = len(self.array) - 1 
@@ -59,25 +28,3 @@ class SearchTree(list):
 
             else:
                 return False 
-
-test = SearchTree()
-
-for i in range(0,2000, 3):
-    test.array.append(i)
-
-print test.array
-print ""
-
-#print test.search(1)
-
-newtest = []
-
-for i in range(0, 2000):
-    if test.search(i):
-        newtest.append(i)
-    elif test.search(i) == 0:
-        newtest.append(i)
-
-print newtest
-
-#print test.search(0)
