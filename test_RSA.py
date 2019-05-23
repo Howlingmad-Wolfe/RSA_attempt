59dbbf90ea9e36ed1dee058b949ddfa741bda04f
Howlingmad-Wolfe <amadwolfe@gmail.com>
2019-05-23 15:57:45 -0500

    Added a test for RSA_key.hash() .unhash()

--- test_RSA.py
+++ test_RSA.py
@@ -25,8 +25,13 @@ class TestRSA(unittest.TestCase):
 
 
     def test_Hash_Unhash(self):
-        
-        pass
+        # test for messages shorter than lenght(modulus - 11 bytes) simulatedt AES 256 key.
+        sim_AES256_key = int(os.urandom(32).encode("hex"), 16)
+        hashed_sim_AES256_key = self.test_RSA_key.hash( sim_AES256_key )
+        hashed_sim_AES256_key = self.test_RSA_key.hash( sim_AES256_key )
+        self.assertEqual( hashed_sim_AES256_key == sim_AES256_key, False ) # hashed message should not equal the cleartext message.
+        self.assertEqual(len( str( hashed_sim_AES256_key ) ), len( str ( self.test_RSA_key.modulus ) ) ) # checking if hashed message is approx. the length of modulus
+        self.assertEqual( self.test_RSA_key.unhash( hashed_sim_AES256_key ), sim_AES256_key ) # unhashed message should be the same as cleartext message.
 
 
 if __name__ == '__main__':
